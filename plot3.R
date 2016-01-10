c558e3d46549706cfa8b105c8ed420d91542490e
Oleksii Ignatenko <o.ignatenko@gmail.com>
2016-01-11 00:25:59 +0200

    Assignment 1

---
+++ plot3.R
@@ -0,0 +1,10 @@
+EPCdata <- read.table(file = "/home/ignat/R/MyCode/household_power_consumption.txt", header = T, sep = ";", col.names = c("Date", "Time", "GlobalAP","GlobalRP","Voltage","GlobalI","Sub1","Sub2","Sub3"),na.strings = "?", nrows = 2880, skip = 66636,stringsAsFactors = FALSE)
+
+y <- data.frame(strptime(paste(EPCdata$Date,EPCdata$Time),format = "%d/%m/%Y %H:%M:%S"),EPCdata$Sub1,EPCdata$Sub2,EPCdata$Sub3)
+names(y)<-c("Time","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+png("/home/ignat/R/MyCode/plot3.png",width=480,height=480,units="px",res=72)
+plot(y$Time,y$Sub_metering_1, type = "l",ylab = "Energy sub metering",xlab = "")
+lines(y$Time,y$Sub_metering_2,col = "red")
+lines(y$Time,y$Sub_metering_3,col = "blue")
+legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),x.intersp = 0.6, cex = 1,bty = "o",col = c("black","red","blue"), lty = c(1,1,1))
+dev.off()
